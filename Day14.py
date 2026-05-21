620ae56d63ef9944d9da18226a8baecf9809d4d2
vanshika-0708 <vanshiki0781@gmail.com>
2026-05-21 13:14:48 +0530

    Day14: Add expense category breakdown pie chart

---
+++ Day14.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+categories=["FOOD","TRAVEL","SHOPPING"]
+expenses=[500,300,200]
+explode=(0.1,0,0)
+#colors
+colors=['yellow','green','blue']
+plt.figure(figsize=(7,7))
+plt.pie(expenses,labels=categories,
+        autopct='%1.if%%',
+        colors=colors,startangle=140,shadow=True)
+plt.title("Category breakdown-Expense Distribution",fontsize=14,fontweight="bold")
+plt.tight_layout()
+plt.show()
